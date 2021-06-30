e63ae84ae7f0b6d5ca93947bb4e1f4eac3e20ccd
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-06-30 15:40:33 +0530

    add social login button

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -4,7 +4,6 @@ import { Redirect } from 'react-router-dom';
 import { signin, authenticate, isAuthenticated } from '../auth/helper/index';
 import { Link } from 'react-router-dom';
 
-
 const Signin = () => {
   const [values, setValues] = useState({
     email: '',
@@ -41,41 +40,80 @@ const Signin = () => {
 
   const signinForm = () => {
     return (
-     <div className="container">
-    <div className="row">
-      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div className="card card-signin my-5">
-          <div className="card-body">
-            <h5 className="card-title text-center">Sign In</h5>
-            <form className="form-signin">
-              <div className="form-label-group">
-                <input onChange={handleChange('email')}
-                value={email} type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
-                <label for="inputEmail">Email address</label>
-              </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">Sign In</h5>
+                <form className="form-signin">
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('email')}
+                      value={email}
+                      type="email"
+                      id="inputEmail"
+                      className="form-control"
+                      placeholder="Email address"
+                      required
+                      autoFocus
+                    />
+                    <label for="inputEmail">Email address</label>
+                  </div>
 
-              <div className="form-label-group">
-                <input onChange={handleChange('password')}
-                value={password} type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-                <label for="inputPassword">Password</label>
-              </div>
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('password')}
+                      value={password}
+                      type="password"
+                      id="inputPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                    />
+                    <label for="inputPassword">Password</label>
+                  </div>
 
-              <div className="custom-control custom-checkbox mb-3">
-                <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                <label className="custom-control-label" for="customCheck1">Remember password</label>
+                  <div className="custom-control custom-checkbox mb-3">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="customCheck1"
+                    />
+                    <label className="custom-control-label" for="customCheck1">
+                      Remember password
+                    </label>
+                  </div>
+                  <button
+                    onClick={onSubmit}
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                  <hr className="my-4" />
+                  {/* <button
+                    class="btn btn-lg btn-google btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i class="fab fa-google mr-2"></i> Sign in with Google
+                  </button>
+                  <button
+                    class="btn btn-lg btn-facebook btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook
+                  </button> */}
+                  <Link className="d-block text-center mt-2 small" to="/signup">
+                    Don't have an account? Sign Up
+                  </Link>
+                </form>
               </div>
-              <button onClick={onSubmit} className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-              <hr className="my-4"/>
-              <Link className="d-block text-center mt-2 small" to="/signup">Don't have an account? Sign Up</Link>
-
-              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-
-      );
+    );
   };
 
   const performRedirect = () => {
--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -17,12 +17,12 @@ const Signup = () => {
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
-    
-  if(name==='confirm_password' && event.target.value != password) {
-    event.target.setCustomValidity("Passwords Don't Match");
-  } else {
-    event.target.setCustomValidity('');
-  }
+
+    if (name === 'confirm_password' && event.target.value != password) {
+      event.target.setCustomValidity("Passwords Don't Match");
+    } else {
+      event.target.setCustomValidity('');
+    }
   };
 
   const onSubmit = (event) => {
@@ -45,52 +45,104 @@ const Signup = () => {
           });
         }
       })
-      .catch((err)=>console.log('Error in signup',err));
+      .catch((err) => console.log('Error in signup', err));
   };
 
   const signupForm = () => {
     return (
-<div className="container">
-    <div className="row">
-      <div className="col-lg-10 col-xl-9 mx-auto">
-        <div className="card card-signin flex-row my-5">
-          <div className="card-img-left d-none d-md-flex">
-            
-          </div>
-          <div className="card-body">
-            <h5 className="card-title text-center">Register</h5>
-            <form className="form-signin">
-              <div className="form-label-group">
-                <input  onChange={handleChange('name')} value={name} type="text" id="inputUserame" className="form-control" placeholder="Username" required autoFocus/>
-                <label for="inputUserame">Name</label>
-              </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-10 col-xl-9 mx-auto">
+            <div className="card card-signin flex-row my-5">
+              <div className="card-img-left d-none d-md-flex"></div>
+              <div className="card-body">
+                <h5 className="card-title text-center">Register</h5>
+                <form className="form-signin">
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('name')}
+                      value={name}
+                      type="text"
+                      id="inputUserame"
+                      className="form-control"
+                      placeholder="Username"
+                      required
+                      autoFocus
+                    />
+                    <label for="inputUserame">Name</label>
+                  </div>
 
-              <div className="form-label-group">
-                <input onChange={handleChange('email')} value={email} type="email" id="inputEmail" className="form-control" placeholder="Email address" required/>
-                <label for="inputEmail">Email address</label>
-              </div>
-              
-              <hr/>
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('email')}
+                      value={email}
+                      type="email"
+                      id="inputEmail"
+                      className="form-control"
+                      placeholder="Email address"
+                      required
+                    />
+                    <label for="inputEmail">Email address</label>
+                  </div>
 
-              <div className="form-label-group">
-                <input  onChange={handleChange('password')} value={password} type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-                <label for="inputPassword">Password</label>
-              </div>
-              
-              <div className="form-label-group">
-                <input onChange={handleChange('confirm_password')} value={confirm_password} type="password" id="inputConfirmPassword" className="form-control" placeholder="Password" required/>
-                <label for="inputConfirmPassword">Confirm password</label>
-              </div>
+                  <hr />
 
-              <button onClick={onSubmit} className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Register</button>
-              <Link className="d-block text-center mt-2 small" to="/signin">Already have an account? Sign In</Link>
-              <hr className="my-4"/>
-              </form>
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('password')}
+                      value={password}
+                      type="password"
+                      id="inputPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                    />
+                    <label for="inputPassword">Password</label>
+                  </div>
+
+                  <div className="form-label-group">
+                    <input
+                      onChange={handleChange('confirm_password')}
+                      value={confirm_password}
+                      type="password"
+                      id="inputConfirmPassword"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                    />
+                    <label for="inputConfirmPassword">Confirm password</label>
+                  </div>
+
+                  <button
+                    onClick={onSubmit}
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                  >
+                    Register
+                  </button>
+
+                  <hr className="my-4" />
+                  {/* <button
+                    class="btn btn-lg btn-google btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i class="fab fa-google mr-2"></i> Sign up with Google
+                  </button>
+                  <button
+                    class="btn btn-lg btn-facebook btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook
+                  </button> */}
+                  <Link className="d-block text-center mt-2 small" to="/signin">
+                    Already have an account? Sign In
+                  </Link>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
     );
   };
 
